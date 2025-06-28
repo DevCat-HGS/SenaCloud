7b459d680c8b7250f72d2186b5721f30830332ed
DevCat-HGS <prototiphs0108@gmail.com>
2025-06-27 19:37:43 -0500

    cambios updaate

--- src/User/Login.tsx
+++ src/User/Login.tsx
@@ -54,15 +54,9 @@ export default function Login() {
                 aria-label={showPassword ? 'Ocultar contraseña' : 'Ver contraseña'}
               >
                 {showPassword ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-green-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                  </svg>
+                  <img src="https://api-img-hgs.netlify.app/img/ver.png" alt="Ocultar contraseña" className="w-5 h-5" />
                 ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-green-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-5.523 0-10-4.477-10-10 0-1.657.403-3.22 1.125-4.575m1.45-2.05A9.956 9.956 0 0112 3c5.523 0 10 4.477 10 10 0 1.657-.403 3.22-1.125 4.575m-1.45 2.05A9.956 9.956 0 0112 21c-2.21 0-4.267-.72-5.925-1.95M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <line x1="4.5" y1="19.5" x2="19.5" y2="4.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-                  </svg>
+                  <img src="https://api-img-hgs.netlify.app/img/ocultar.png" alt="Ver contraseña" className="w-5 h-5" />
                 )}
               </button>
             </div>
