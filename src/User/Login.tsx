459c4867c1f753eb5fd1fb9f92fdf98b591ace99
DevCat-HGS <prototiphs0108@gmail.com>
2025-06-30 17:00:38 -0500

    avances

--- src/User/Login.tsx
+++ src/User/Login.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../img/Logo.svg';
 
 export default function Login() {
   const [form, setForm] = useState({
